6ac4737904452133afc91d271a7756f41bca754a
pirukusu <pirukusu1515@gmail.com>
2025-05-14 08:00:28 +0900

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,15 @@
+export const onRequest = async ({ request, next }) => {
+  const referer = request.headers.get("referer") || "";
+
+  // FANBOXからのアクセス以外は拒否
+  if (!referer.includes("fanbox.cc")) {
+    return new Response("403 Forbidden - No hotlinking allowed", {
+      status: 403,
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    });
+  }
+
+  return next();
+};
