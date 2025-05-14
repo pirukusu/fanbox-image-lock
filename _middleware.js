27d7d6396735598746bbf90d8e99cf7ceb76d607
pirukusu <pirukusu1515@gmail.com>
2025-05-14 09:21:06 +0900

    Update _middleware.js

--- _middleware.js
+++ _middleware.js
@@ -1,13 +1,1 @@
-export async function onRequest({ request }) {
-  const referer = request.headers.get("Referer");
-  const url = new URL(request.url);
 
-  // 画像は制限なし
-  if (url.pathname.endsWith(".png") || url.pathname.endsWith(".jpg")) {
-    return;
-  }
-
-  if (!referer || !referer.includes("fanbox.cc")) {
-    return new Response("Forbidden", { status: 403 });
-  }
-}
