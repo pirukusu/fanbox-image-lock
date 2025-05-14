720bebdaf1f238413bbdd933a4888ce2e0af49b8
pirukusu <pirukusu1515@gmail.com>
2025-05-14 09:17:19 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,15 @@
-export const onRequest = async ({ request, next }) => {
+export const onRequest = async ({ request, next }) =&gt; {
   const referer = request.headers.get("referer") || "";
+  const url = new URL(request.url);
 
-  // FANBOXからのアクセス以外は拒否
-  if (!referer.includes("fanbox.cc")) {
+  // 画像ファイルは制限せず常に表示OK
+  const allowedExtensions = [".png", ".jpg", ".jpeg", ".webp", ".gif"];
+  if (allowedExtensions.some(ext =&gt; url.pathname.endsWith(ext))) {
+    return next();
+  }
+
+  // その他のリクエストはFANBOXからのみ許可
+  if (!referer.includes("http://fanbox.cc")) {
     return new Response("403 Forbidden - No hotlinking allowed", {
       status: 403,
       headers: {
@@ -11,5 +18,6 @@ export const onRequest = async ({ request, next }) => {
     });
   }
 
+  // 問題なければ通常どおり処理
   return next();
 };
